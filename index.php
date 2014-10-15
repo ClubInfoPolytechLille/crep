d8c7aa8abca3e4403455f170daa9426691b6a75b
Jean Wasilewski <henyxia@live.fr>
2014-10-15 18:38:14 +0200

    UTF8 encoding

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE>
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title>Site Web de la Coupe de Robotique des Ecoles Primaires</title>
 	</head>
 	<body>
