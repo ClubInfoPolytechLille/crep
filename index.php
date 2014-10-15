096a903b782af02ac0efee287c0e939ebf4d29bc
Jean Wasilewski <henyxia@live.fr>
2014-10-15 18:46:20 +0200

    Description and author added

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Site Web de la Coupe de Robotique des Ecoles Primaires">
+		<meta name="author" content="Club Informatique de PolytechLille">
 		<title>Site Web de la Coupe de Robotique des Ecoles Primaires</title>
 	</head>
 	<body>
