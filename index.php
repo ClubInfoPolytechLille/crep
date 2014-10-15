66d6b134e311849f5afa9680b83f1e2ca7706246
Jean Wasilewski <henyxia@live.fr>
2014-10-15 18:50:39 +0200

    Added viewport meta

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="Site Web de la Coupe de Robotique des Ecoles Primaires">
 		<meta name="author" content="Club Informatique de PolytechLille">
