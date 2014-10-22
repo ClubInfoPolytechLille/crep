b94caab9ce838bfd2ba8d6bf12b0aba6800b2e14
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-10-22 13:04:02 +0200

    ajout de commentaire

--- topnavbar.php
+++ topnavbar.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">CREP</a> <!-- lien pour actualiser + message quand la souris passe desous -->
+      <a class="navbar-brand" href="#">CREP</a> <!-- ajouter un lien pour actualiser + message [Actualiser] ou [Coupe de Robotique des Ecoles Primaires] quand la souris passe -->
     </div>
     <div class="collapse navbar-collapse">
     
-    <!-- modifier le test -->
+    
       <?php
-        if ( 0 == 1 ) 
+        if ( 0 == 1 ) // modifier le test
           {
             ?>
               <form class="navbar-form navbar-right" role="search">
-                <button type="submit" class="btn btn-default">Inscription</button> <!-- lien pour s inscrire  -->
-                <button type="submit" class="btn btn-default">Connection</button> <!-- lien pour se connecter -->
+                <button type="submit" class="btn btn-default">Inscription</button> <!-- ajouter un lien pour s inscrire  -->
+                <button type="submit" class="btn btn-default">Connection</button> <!-- ajouter un lien pour se connecter -->
               </form>
             <?php
           }
@@ -20,7 +20,7 @@
           {
             ?>
               <p class="navbar-text navbar-right">Bienvenue, 
-                <a href="#" class="navbar-link">NOM DE LA PERSONNE</a> <!-- aller au profil ? -->
+                <a href="#" class="navbar-link">NOM DE LA PERSONNE</a> <!-- ajouter un lien aller au profil ? -->
               </p>
             <?php
           }
