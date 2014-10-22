06081f6d7a71f4f2bf8cd61f2d9a06a7cdb42eef
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-10-22 12:24:04 +0200

    creation de tobnavbar.php (pour la barre de navigation)

---
+++ topnavbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-fixed-top" role="navigation">
+  <div class="container">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="#">Titre</a>
+  </div>
+  <div class="collapse navbar-collapse">
+  
+  <?php>
+    if ( 1 == 1 )
+      {    
+        <form class="navbar-form navbar-right" role="search">
+          <button type="submit" class="btn btn-default">Inscription</button>
+          <button type="submit" class="btn btn-default">Connection</button>
+        </form>
+      }
+    else
+      {
+        <p class="navbar-text navbar-right">Bienvenue,_
+          <a href="#" class="navbar-link">enanrock</a>
+        </p>
+      }
+      </div>
+  <?>
+  
+  </div>
+</nav>
