392aea79ebbd935537e549e285ad732d560fa5bc
Jean Wasilewski <henyxia@live.fr>
2014-10-22 12:51:48 +0200

    Added database name and creds.php to index.php

--- creds.php
+++ creds.php
@@ -2,3 +2,4 @@
 define("__MYSQL_USERNAME__", "crep");
 define("__MYSQL_PASSWORD__", "lescrepcestcool");
 define("__MYSQL_HOSTNAME__", "cambraisis.escaut.net");
+define("__MYSQL_DATABASE__", "crep");
