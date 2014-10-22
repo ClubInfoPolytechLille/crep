357eb4ff20b45ef4c374704695e8fdc7d332cd02
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-10-22 12:59:29 +0200

    remplacer _ par un espace

--- topnavbar.php
+++ topnavbar.php
@@ -1,26 +1,26 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">CREP</a>
+      <a class="navbar-brand" href="#">CREP</a> <!-- lien pour actualiser + message quand la souris passe desous -->
     </div>
     <div class="collapse navbar-collapse">
     
-    
+    <!-- modifier le test -->
       <?php
-        if ( 0 == 1 )
+        if ( 0 == 1 ) 
           {
             ?>
               <form class="navbar-form navbar-right" role="search">
-                <button type="submit" class="btn btn-default">Inscription</button>
-                <button type="submit" class="btn btn-default">Connection</button>
+                <button type="submit" class="btn btn-default">Inscription</button> <!-- lien pour s inscrire  -->
+                <button type="submit" class="btn btn-default">Connection</button> <!-- lien pour se connecter -->
               </form>
             <?php
           }
         else
           {
             ?>
-              <p class="navbar-text navbar-right">Bienvenue,_
-                <a href="#" class="navbar-link">NOM DE LA PERSONNE</a>
+              <p class="navbar-text navbar-right">Bienvenue, 
+                <a href="#" class="navbar-link">NOM DE LA PERSONNE</a> <!-- aller au profil ? -->
               </p>
             <?php
           }
