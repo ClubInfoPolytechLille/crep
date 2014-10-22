4b49f7a044b954f74218d5e26a82ba40ac993b53
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-10-22 12:51:15 +0200

    corection dans le test if en ajoutant <?php ... ?> dans les deux { } (2eme fois)

--- topnavbar.php
+++ topnavbar.php
@@ -1,27 +1,31 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
   <div class="container">
-  <div class="navbar-header">
-    <a class="navbar-brand" href="#">CREP</a>
-  </div>
-  <div class="collapse navbar-collapse">
-  
-
-  <?php
-    if ( 0 == 1 )
-      {    
-        <form class="navbar-form navbar-right" role="search">
-          <button type="submit" class="btn btn-default">Inscription</button>
-          <button type="submit" class="btn btn-default">Connection</button>
-        </form>
-      }
-    else
-      {
-        <p class="navbar-text navbar-right">Bienvenue,_
-          <a href="#" class="navbar-link">NOM DE LA PERSONNE</a>
-        </p>
-      }
-      </div>
-   ?>
-  
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">CREP</a>
+    </div>
+    <div class="collapse navbar-collapse">
+    
+    
+      <?php
+        if ( 0 == 1 )
+          {
+            ?>
+              <form class="navbar-form navbar-right" role="search">
+                <button type="submit" class="btn btn-default">Inscription</button>
+                <button type="submit" class="btn btn-default">Connection</button>
+              </form>
+            <?php
+          }
+        else
+          {
+            ?>
+              <p class="navbar-text navbar-right">Bienvenue,_
+                <a href="#" class="navbar-link">NOM DE LA PERSONNE</a>
+              </p>
+            <?php
+          }
+      ?>
+      
+    </div>
   </div>
 </nav>
