475ba9de013da7a003240f40caae78eb6f25e52f
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-10-24 16:26:09 +0200

    modification pour les liens et test

--- topnavbar.php
+++ topnavbar.php
@@ -11,8 +11,8 @@
         {
           ?>
             <form class="navbar-form navbar-right" role="search">
-              <button type="submit" class="btn btn-default">Inscription</button> <!-- ajouter un lien pour s inscrire  -->
-              <button type="submit" class="btn btn-default">Connection</button> <!-- ajouter un lien pour se connecter -->
+              <input type="text" placeholder="Inscription_1" name="Inscription_2" /><br /><!-- ajouter un lien pour s inscrire  -->
+              <input type="text" placeholder="Connection_1" name="Connection_2" /><br /><!-- ajouter un lien pour se connecter -->
             </form>
           <?php
         }
@@ -22,7 +22,7 @@
           {
             ?>
               <form class="navbar-form navbar-right" action="prog.php" method=POST>
-                <input type="text" placeholder="identifient" name="identifient" /><br />
+                <input type="text" placeholder="identifient_1" name="identifient_2" /><br />
                 <input type="text" placeholder="mot de passe" name="mot_de_passe" /><br /> <!-- mettre des ***** quand on entre le mot de passe  -->
                 <input type="submit" value="se connecter" />
               </form>
