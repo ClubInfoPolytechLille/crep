7ab7e900ad2c63854d04230287056393f73cd8c4
Jean Wasilewski <henyxia@live.fr>
2014-10-31 11:27:24 +0100

    file function added

--- js/crep.js
+++ js/crep.js
@@ -1,3 +1,19 @@
+function file(fichier)
+{
+	if(window.XMLHttpRequest)
+		xhr_object = new XMLHttpRequest();
+	else if(window.ActiveXObject)
+		xhr_object = new ActiveXObject("Microsoft.XMLHTTP");
+	else
+		return(false);
+	xhr_object.open("GET", fichier, false);
+	xhr_object.send(null);
+	if(xhr_object.readyState == 4)
+		return(xhr_object.responseText);
+    else
+		return(false);
+}
+
 function loadNewDoc(doc)
 {
 	$("#mainContainer").html(file(doc));
