2a6659d8079a3eadd5cc56d91eef60be904e6f51
Jean Wasilewski <henyxia@live.fr>
2014-10-31 12:16:13 +0100

    Switched to well style

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-<div class="bs-docs-sidebar hidden-print hidden-xs hidden-sm affix-top" role="complementary">
+<div class="well well-sm" role="complementary">
 	<ul class="nav bs-docs-sidenav">
 		<li>
 			<a href="#js-overview">Connexion</a>
