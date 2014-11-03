95b03deea3d957c0e89320fca5dcba85a31e7e06
Jean Wasilewski <henyxia@live.fr>
2014-11-03 13:29:49 +0100

    Connection to Mysql fixed

--- news.php
+++ news.php
@@ -1,7 +1,7 @@
 <div class="jumbotron">
 <?php 
         require_once("creds.php");
-	$link = mysql_connect(MYSQL_HOSTNAME, MYSQL_USER, MYSQL_PASSWORD)
+	$link = mysql_connect(__MYSQL_HOSTNAME__, __MYSQL_USER__, __MYSQL_PASSWORD__)
 		or die("Impossible de se connecter : " . mysql_error());
 	echo 'Connexion réussie';
 	
