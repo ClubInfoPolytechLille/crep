0e151ddbe9e5696037f7d5b3d6c4d95e973372c2
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-11-05 13:10:40 +0100

    creation de la page se connect

--- connect.php
+++ connect.php
@@ -1,1 +1,28 @@
-test
+<form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label for="entrer_Identifiant_3" class="col-sm-2 control-label">Identifiant</label>
+    <div class="col-sm-10">
+      <input type="identifiant" class="form-control" id="Entrer_Identifiant_3" placeholder="Votre identifiant">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="entrer_mot_de_passe_3" class="col-sm-2 control-label">Mot de passe</label>
+    <div class="col-sm-10">
+      <input type="mot de passe" class="form-control" id="entrer_mot_de_passe_3" placeholder="Votre mot de passe">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Rester connecter
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Se connecter</button>
+    </div>
+  </div>
+</form>
