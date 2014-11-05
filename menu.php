240b629b21bb0bdad42c1f4738424c0afb668008
Jean Wasilewski <henyxia@live.fr>
2014-11-05 12:56:58 +0100

    Fixed menu

--- menu.php
+++ menu.php
@@ -1,16 +1,16 @@
 <div class="well well-sm" role="complementary">
 	<ul class="nav">
 		<li>
-			<a href="#" onClick="file('news.php')">News</a>
+			<a href="#" onClick="file('news.php'):">News</a>
 		</li>
 		<li>
-			<a href="#" onClick="file('agenda.php')">Agenda</a>
+			<a href="#" onClick="file('agenda.php');">Agenda</a>
 		</li>
 		<li>
-			<a href="#" onClick="file('orga.php')">Organisation</a>
+			<a href="#" onClick="file('orga.php');">Organisation</a>
 		</li>
 		<li>
-			<a href="#" onClick="file('connect.php')">Connexion</a>
+			<a href="#" onClick="file('connect.php;')">Connexion</a>
 		</li>
 	</ul>
 </div>
