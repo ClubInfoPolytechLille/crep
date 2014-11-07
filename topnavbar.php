475c2deb3dce3d865200c328521abf5d5631e90f
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-11-07 17:12:31 +0100

    test pour le logo v9

--- topnavbar.php
+++ topnavbar.php
@@ -6,7 +6,7 @@
         <img data-src="logo-895x1030.js/895x1030" alt="...">
       </a>
       
-      <a class="navbar-brand" onClick="document.location.reload();" data-toggle="tooltip" data-placement="bottom" title="Afficher la page d'accueil" href="#">Coupe de Robotique des Ecoles Primaires</a> 
+      <!-- <a class="navbar-brand" onClick="document.location.reload();" data-toggle="tooltip" data-placement="bottom" title="Afficher la page d'accueil" href="#">Coupe de Robotique des Ecoles Primaires</a> -->
     </div>
     <div class="collapse navbar-collapse">
       <?php
