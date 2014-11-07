2ba3f2a495c4efd49a442426f3ac6b71b40ec343
Erwan Nanrocki <erwan.nanrocki@hotmail.fr>
2014-11-07 16:31:57 +0100

    ajout des *** quand on ecrit le mot de passe

--- connect.php
+++ connect.php
@@ -1,16 +1,19 @@
 <form class="form-horizontal" role="form">
+  
   <div class="form-group">
     <label for="entrer_Identifiant_3" class="col-sm-2 control-label">Identifiant</label>
     <div class="col-sm-10">
       <input type="identifiant" class="form-control" id="Entrer_Identifiant_3" placeholder="Votre identifiant">
     </div>
   </div>
+
   <div class="form-group">
-    <label for="entrer_mot_de_passe_3" class="col-sm-2 control-label">Mot de passe</label>
+    <label class="sr-only" for="entrer_mot_de_passe_3">Mot de passe</label> <!-- "col-sm-2 control-label" ou "sr-only" -->
     <div class="col-sm-10">
       <input type="mot de passe" class="form-control" id="entrer_mot_de_passe_3" placeholder="Votre mot de passe">
     </div>
   </div>
+  
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <div class="checkbox">
@@ -20,10 +23,12 @@
       </div>
     </div>
   </div>
+  
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">Se connecter</button> <!-- Il faut ajouter le lien d'identification  -->
     </div>
   </div>
+  
 </form>
 
