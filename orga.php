f84e30e374598278ad994af5d0422edb375d5760
Geoffrey Frogeye <geoffrey@frogeye.fr>
2014-11-08 20:43:38 +0100

    [Orga] Retravail
    - Nouveau design
    - Classe Evenement

--- orga.php
+++ orga.php
@@ -4,49 +4,134 @@
 # DEBUG
 # e_ : est
 # r_ : a le droit de
-$e_connecte = true;
-$r_voir = $e_connecte;
-$r_vote = true;
-$r_proposer = true;
 
-if ($r_voir) {
-	if ($r_vote) {
-		if ($r_proposer) {
-?>
-<h3>Ajouter un évènement</h3>
-<form id="nvEv">
-	<label for="nom">Nom :</label><br/><input type="text" name="nom"><br/>
-	<label for="desc">Description :</label><br/><textarea name="desc"></textarea><br/>
-	<h4>Dates proposées</h4>
-	<div id="nvEvDates">
-		Pas de date proposée pour l'instant.
-	</div>
-	<form id="nvDate" action="#">
-		<h5>Proposer une date</h5>
-		<label for="nvDateDate">Date :</label><br/><input type="datetime"name="nvDateDate"/><br/>
-		<label for="nvDateDuree">Durée :</label><br/><input type="time"name="nvDateDuree"/><br/>
-		<input class="btn btn-default" type="submit" value="Proposer la date">
-	</form>
-	<input class="btn btn-primary" type="submit" value="Ajouter l'évènement">
-</form>
-<?php
-		}
-?>
-<h3>Évènements à planifier</h3>
-<div class="ev">
+$e_connecte = false;
+$e_modo = true;
 
-</div>
-<?php
-	}
-?>
-<h3>Évènements fixés</h3>
-<?php
-} else {
-?>
-<p class="bg-danger">
-Vous devez vous connecter pour gérer l'organisation des évènements.
-</p>
-<?php
+$droits  = array('voir', 'voter', 'ajouter', 'proposer', 'annuler', 'supprimer', 'modifier', 'valider');
+
+class Evenement
+{
+    public $id = 0;
+    public $nom = "Sans nom";
+    public $description = "Sans description";
+    public $annule = false;
+    public $valide = false;
+    public $duree = 120;
+
+    public $dates = array();
+
+    public function html() {
+        global $droits;
+
+        $html = '   <li class="list-group-item">';
+
+        # Titre
+        $html .= '<h3 class="list-group-item-heading">'.$this->nom;
+        if ($this->annule) {
+            $html .= ' <span class="label label-danger">Annulé</span>';
+        }
+        if (in_array('annuler', $droits) && !$this->annule) { # TODO
+            $html .= ' <button type="button" class="btn btn-danger"><span class="glyphicon glyphicon glyphicon-remove"></span></button>';
+        }
+        if (in_array('supprimer', $droits) && !$this->valide) { # TODO
+            $html .= ' <button type="button" class="btn btn-danger"><span class="glyphicon glyphicon glyphicon-trash"></span></button>';
+        }
+        $html .= '</h3>';
+
+        # Description
+        $html .= '<div class="panel panel-default">';
+        $html .= '<div class="panel-heading">';
+        $html .= '<h4 class="panel-title">Description';
+        if (in_array('modifier', $droits)) {
+            $html .= ' <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span></button>';
+        }
+        $html .= '</h4>';
+        $html .= '</div>';
+        $html .= '<div class="panel-body">';
+        $html .= '<p>';
+        $html .= nl2br(htmlspecialchars($this->description));
+        $html .= '</p>';
+        // $html .= '<hr/>';
+        $html .= '<p>';
+        $html .= 'Durée : '.$this->duree.' minutes <br/>';
+        if ($this->valide) {
+            $html .= 'Date : '.date('c', $this->valide).'<br/>';
+        }
+        $html .= '</p>';
+        if ($this->annule) {
+            $html .= '<p class="alert alert-danger" role="alert">Annulé</p>';
+
+        }
+        $html .= '</div>';
+        $html .= '</div>';
+
+        # Dates
+        if (!$this->valide && !$this->annule) {
+            $html .= '<div class="panel panel-default">';
+            $html .= '<div class="panel-heading">';
+            $html .= '<h4 class="panel-title">Dates possibles';
+            if (in_array('proposer', $droits)) {
+                $html .= '<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-plus"></span></button>';
+            }
+            $html .= '</h4>';
+            $html .= '</div>';
+            $html .= '<div class="panel-body">';
+            $html .= '<ul class="list-group">';
+            foreach ($this->dates as $dateIndex => $date) {
+                $html .= '<li class="list-group-item">'.date('c', $date).' (6 <span class="glyphicon glyphicon-user"></span>)</li>';
+            }
+            $html .= '</ul>';
+            if (in_array('valider', $droits) && !$this->valide) {
+                $html .= '<p><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span> Valider la date</button></p>';
+            }
+            $html .= '</div>';
+            $html .= '</div>';
+        }
+
+        $html .= '</li>';
+        return $html;
+    }
 }
 
 ?>
+
+<h1>Évènements</h1>
+
+<h2>Plannifiés <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span></button></h2>
+<ul class="list-group">
+<?
+$test1 = new Evenement;
+$test1->dates[] = time();
+$test1->valide = time();
+echo $test1->html()
+?>
+<?
+$test2 = new Evenement;
+$test2->dates[] = time();
+$test2->valide = time();
+$test2->annule = true;
+echo $test2->html()
+?>
+</ul>
+
+
+<h2>À plannifier <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span></button></h2>
+<ul class="list-group">
+<?
+$test3 = new Evenement;
+$test3->dates[] = time();
+echo $test3->html()
+?>
+<?
+$test4 = new Evenement;
+$test4->dates[] = time();
+$test4->annule = true;
+echo $test4->html()
+?>
+</ul>
+
+
+<h2>Évènements passés</h2>
+<ul class="list-group">
+</ul>
