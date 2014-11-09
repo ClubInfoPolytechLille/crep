e4ccf087e125dbbcf8a61140b5dcb4d598710032
Jean Wasilewski <henyxia@live.fr>
2014-11-09 16:27:20 +0100

    New logout icon

--- topnavbar.php
+++ topnavbar.php
@@ -15,7 +15,7 @@
 
 ?>
 		<p class="navbar-text navbar-right">
-			Bienvenue, <a href="#" class="navbar-link"><?php echo $_SESSION["realname"];?>.'</a>
+			Bienvenue, <a href="#" class="navbar-link"><?php echo $_SESSION["realname"];?> </a>
 			<a href="#" onclick="file('logout.php');document.location.reload();" class="glyphicon glyphicon-poweroff"></a>
 
 		</p>
