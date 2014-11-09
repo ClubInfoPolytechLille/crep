f42e5a0f6f073152c6d912da460f7d640952f242
Jean Wasilewski <henyxia@live.fr>
2014-11-09 15:48:24 +0100

    Fixed issue with die

--- checkCreds.php
+++ checkCreds.php
@@ -7,7 +7,7 @@ $link = mysql_connect(__MYSQL_HOSTNAME__, __MYSQL_USERNAME__, __MYSQL_PASSWORD__
 	or die("Nop");
 
 if(!mysql_select_db('crep', $link))
-	die 'Nop';
+	die('Nop');
 
 $requete = 'SELECT pk, admin FROM users WHERE username=\''.$_GET["user"].'\' AND password=\''.$_GET["password"].'\'';
 $resultat = mysql_query($requete);
