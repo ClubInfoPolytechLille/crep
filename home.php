a6700e368d5cba7383f8f8588dad72aca3cec461
henyxia <henyxia@live.fr>
2015-02-26 17:05:31 +0100

    A more responsive carousel

--- home.php
+++ home.php
@@ -1,8 +1,10 @@
-<div class="carousel slide" data-ride="carousel">
+<div id="homecarousel" class="carousel slide" data-ride="carousel">
 	<ol class="carousel-indicators">
-		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		<li data-target="#homecarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#homecarousel" data-slide-to="1"></li>
+		<li data-target="#homecarousel" data-slide-to="2"></li>
+		<li data-target="#homecarousel" data-slide-to="3"></li>
+		<li data-target="#homecarousel" data-slide-to="4"></li>
 	</ol>
 	<div class="carousel-inner" role="listbox">
 		<div class="item active">
@@ -36,11 +38,11 @@
 			</div>
 		</div>
 	</div>
-	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	<a class="left carousel-control" href="#homecarousel" role="button" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 		<span class="sr-only">Previous</span>
 	</a>
-	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	<a class="right carousel-control" href="#homecarousel" role="button" data-slide="next">
 		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 		<span class="sr-only">Next</span>
 	</a>
