19131152b629fbfe0717263974f0a0636bd5c862
henyxia <henyxia@live.fr>
2015-02-26 16:03:32 +0100

    Caroussel - Final form

--- home.php
+++ home.php
@@ -6,15 +6,33 @@
 	</ol>
 	<div class="carousel-inner" role="listbox">
 		<div class="item active">
-			<img src="..." alt="...">
+			<img src="img/CREP-1.jpg" alt="">
 			<div class="carousel-caption">
-				Cap1
+				Coupe Robotique des Ecoles Primaires - Edition 2015
 			</div>
 		</div>
 		<div class="item">
-			<img src="..." alt="...">
+			<img src="img/CREP-2.jpg" alt="">
 			<div class="carousel-caption">
-				...
+				Projet réalisé sur des robots LEGO MindStorm
+			</div>
+		</div>
+		<div class="item">
+			<img src="img/CREP-3.jpg" alt="">
+			<div class="carousel-caption">
+				300 écoliers venus relever le défi
+			</div>
+		</div>
+		<div class="item">
+			<img src="img/CREP-4.jpg" alt="">
+			<div class="carousel-caption">
+				Visite de l’école : Démonstration du robot NAO
+			</div>
+		</div>
+		<div class="item">
+			<img src="img/CREP-5.jpg" alt="">
+			<div class="carousel-caption">
+				Journée rythmée par des activités animées par les étudiants
 			</div>
 		</div>
 	</div>
