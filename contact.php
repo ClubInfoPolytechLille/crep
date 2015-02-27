791963f055ce17b18960b3c8b5e559ab9402e241
henyxia <henyxia@live.fr>
2015-02-27 11:37:09 +0100

    Another design

--- contact.php
+++ contact.php
@@ -3,20 +3,15 @@
   <p>Pour toute information, vous pouvez contacter Mme Pichonnat</p>
 </div>
 &nbsp;
-<div class="jumbotron">
-	<h1>Comment y accéder ?</h1>
-	<p>
-		<div id="map-canvas"></div>
-	</p>
-</div>
+<div id="map-canvas"></div>
 <script>
 	function initialize()
 	{
 		var mapCanvas = document.getElementById('map-canvas');
 		var mapOptions =
 		{
-			center: new google.maps.LatLng(50.6080275, 3.1401501),
-			zoom: 8,
+			center: new google.maps.LatLng(50.6074998, 3.1373338),
+			zoom: 16,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
         var map = new google.maps.Map(mapCanvas, mapOptions)
