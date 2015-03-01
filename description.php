bf153a987c78f3dca44a342a9d8d25f927dc0bdc
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 12:01:40 +0100

    Fake imgs

--- description.php
+++ description.php
@@ -2,21 +2,21 @@
     <h1>Descriptif détaillé</h1>
     <div class="row">
         <div class="col-md-12">
-            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="img/me.jpg" />
+            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="http://fakeimg.pl/200/" />
             <p>Depuis la rentrée scolaire 2013-2014, Polytech Lille en partenariat avec plusieurs circonscriptions de l’Académie de Lille (Lambersart, Marquette, Saint-André, Wambrechies) organise une initiation à la robotique en école primaire auprès d’élèves de CM1 et CM2. Cette année, la coupe se déroulera le jeudi 9 avril 2015 dans les locaux de Polytech avec 12 classes participantes et récompensera la meilleure équipe.</p>
         </div>
     </div>
 	<h1>Concept</h1>
     <div class="row">
         <div class="col-md-12">
-            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="img/me.jpg" />
+            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="http://fakeimg.pl/200/" />
             <p>Plusieurs membres de Robotech (le club de robotique de Polytech Lille) participent à ce projet pour aider les enfants dans leur démarche de d’initiation. Ils découvrent ainsi le monde de la robotique à l’aide de kits Lego Mindstorm constitués de pièces Lego et d’une « brique » programmable qui sont confiés à la classe. Ce kit permet la construction et la programmation d’un robot à l’aide d’un logiciel de programmation par schémas blocs. Deux étudiants de Polytech Lille interviennent les jeudis après midi dans les différentes classes pour les aider à programmer et à se familiariser avec le logiciel. Après cette phase de découverte, chaque école doit construire et programmer le robot selon un défi donné à toutes les écoles le jour de la CREP.</p>
         </div>
     </div>
 	<h1>Activités</h1>
     <div class="row">
         <div class="col-md-12">
-            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="img/me.jpg" />
+            <img style='float:left;width:200px;height:200px; margin-right:10px;' src="http://fakeimg.pl/200/" />
             <p>Tout au long de cette journée, les classes des écoles primaires vont pouvoir s’amuser avec des jeux sportifs sur la pelouse de Polytech et seront accompagnés d’élèves ingénieurs. Le Bureau Des Etudiants (BDE), la Fanfare, la Sono, les Pompoms et bien sur le club Robotech seront de la partie. La CREP sera également l’occasion pour les enfants de visiter l’école Polytech Lille et ses salles de TP. A la fin de la journée, les élèves s’installeront dans l’amphithéâtre Migeon pour la remise des prix.</p>
         </div>
     </div>
