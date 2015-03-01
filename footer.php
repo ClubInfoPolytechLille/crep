d05ef03b645f90866ab00ab250646b8101031d2f
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 16:54:15 +0100

    Small space below footer
    Looks nicer

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
-<div class="container text-center">
+<div id="footer" class="container text-center">
     <hr />
   <div class="row">
     <div class="col-lg-12">
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
           <li>Sponsorisé par PolytechLille</li>
-          <li><img height="100" src="img/polytechlille.png" /></li>
+          <li><img src="img/polytechlille.png" /></li>
         </ul>
       </div>
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
           <li>Et par l'Université de Lille 1</li>
-          <li><img height="100" src="img/lille1.png" /></li>
+          <li><img src="img/lille1.png" /></li>
         </ul>
       </div>  
     </div>
