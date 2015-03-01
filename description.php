1a48579e5ed29430ab6fd5d482c2e6e12d20e12b
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 14:34:12 +0100

    Correct brand names
    Following http://www.lego.com/en-US/aboutus/lego-group/company-profile

--- description.php
+++ description.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-md-12">
             <img style='float:left;width:200px;height:200px; margin-right:10px;' src="http://fakeimg.pl/200/" />
-            <p>Plusieurs membres de Robotech (le club de robotique de Polytech Lille) participent à ce projet pour aider les enfants dans leur démarche de d’initiation. Ils découvrent ainsi le monde de la robotique à l’aide de kits Lego Mindstorm constitués de pièces Lego et d’une « brique » programmable qui sont confiés à la classe. Ce kit permet la construction et la programmation d’un robot à l’aide d’un logiciel de programmation par schémas blocs. Deux étudiants de Polytech Lille interviennent les jeudis après midi dans les différentes classes pour les aider à programmer et à se familiariser avec le logiciel. Après cette phase de découverte, chaque école doit construire et programmer le robot selon un défi donné à toutes les écoles le jour de la CREP.</p>
+            <p>Plusieurs membres de Robotech (le club de robotique de Polytech Lille) participent à ce projet pour aider les enfants dans leur démarche de d’initiation. Ils découvrent ainsi le monde de la robotique à l’aide de kits LEGO Mindstorms constitués de pièces LEGO et d’une « brique » programmable qui sont confiés à la classe. Ce kit permet la construction et la programmation d’un robot à l’aide d’un logiciel de programmation par schémas blocs. Deux étudiants de Polytech Lille interviennent les jeudis après midi dans les différentes classes pour les aider à programmer et à se familiariser avec le logiciel. Après cette phase de découverte, chaque école doit construire et programmer le robot selon un défi donné à toutes les écoles le jour de la CREP.</p>
         </div>
     </div>
 	<h1>Activités</h1>
