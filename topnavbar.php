d6b3dc4887064ed7b2c4dcdd6ae155385edc410e
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 14:09:26 +0100

    Logo 2013-2014
    But it's so small that I commented it out.

--- topnavbar.php
+++ topnavbar.php
@@ -1,12 +1,8 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
-    <div class="navbar-header">
-      
-      <!-- <a class="thumbnail" onClick="document.location.reload();" href="#">
-        <img data-src="logo-895x1030.jpg/895x1030" alt="...">                           je n'arriva pas a mettre le logo 2013-2014 :'(
-      </a> -->
-      
+    <div class="navbar-header">      
       <a class="navbar-brand" onClick="document.location.reload();" data-toggle="tooltip" data-placement="bottom" title="Aller à la page d'accueil" href="#">
+        <!-- <img src="logo-895x1030.jpg" alt="..." height="30" style="display: inline;"> -->
         <span class="hidden-xs">Coupe de Robotique des Ecoles Primaires</span>
         <span class="visible-xs"><img src="img/logo.png" height="20" /></span>
       </a>
