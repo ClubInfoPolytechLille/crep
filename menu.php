5feb6c55f0c06b379ec889a0373f502cd07f3d8d
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 14:22:04 +0100

    Better menu navbar on small screens
    Should collapse in a menu maybe?
    Still, have a left blank space on the right on Chrome Android,
    but works ok on PC.

--- menu.php
+++ menu.php
@@ -1,23 +1,21 @@
 <nav class="navbar navbar-default navbar-lower">
 	<div class="container-fluid">
-		<div class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li>
-					<a href="#news" onClick="loadNewDoc('home.php');">Accueil</a>
-				</li>
-				<li>
-					<a href="#agenda" onClick="loadNewDoc('description.php');">Descritption</a>
-				</li>
-				<li>
-					<a href="#orga" onClick="loadNewDoc('sponsors.php');">Sponsors</a>
-				</li>
-				<li>
-					<a href="#profil" onClick="loadNewDoc('schools.php);">Ecoles participantes</a>
-				</li>
-				<li>
-					<a href="#school" onClick="loadNewDoc('contact.php');">Contact</a>
-				</li>
-			</ul>
-		</div>
+		<ul class="nav navbar-nav">
+			<li>
+				<a href="#news" onClick="loadNewDoc('home.php');">Accueil</a>
+			</li>
+			<li>
+				<a href="#agenda" onClick="loadNewDoc('description.php');">Descritption</a>
+			</li>
+			<li>
+				<a href="#orga" onClick="loadNewDoc('sponsors.php');">Sponsors</a>
+			</li>
+			<li>
+				<a href="#profil" onClick="loadNewDoc('schools.php);">Ecoles participantes</a>
+			</li>
+			<li>
+				<a href="#school" onClick="loadNewDoc('contact.php');">Contact</a>
+			</li>
+		</ul>
 	</div>
 </nav>
