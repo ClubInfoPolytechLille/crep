f44c2d085282f5f89249ce1d8c2d5a3ddb7368bb
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-01 13:54:37 +0100

    Title collapse on small screens
    Otherwise it overflowed.
    Not sure if I should use the logo or plain CREP

--- topnavbar.php
+++ topnavbar.php
@@ -6,7 +6,10 @@
         <img data-src="logo-895x1030.jpg/895x1030" alt="...">                           je n'arriva pas a mettre le logo 2013-2014 :'(
       </a> -->
       
-      <a class="navbar-brand" onClick="document.location.reload();" data-toggle="tooltip" data-placement="bottom" title="Aller à la page d'accueil" href="#">Coupe de Robotique des Ecoles Primaires</a>
+      <a class="navbar-brand" onClick="document.location.reload();" data-toggle="tooltip" data-placement="bottom" title="Aller à la page d'accueil" href="#">
+        <span class="hidden-xs">Coupe de Robotique des Ecoles Primaires</span>
+        <span class="visible-xs"><img src="img/logo.png" height="20" /></span>
+      </a>
     </div>
 	<div class="collapse navbar-collapse" style="margin-right: 10px;">
 		<p class="navbar-text navbar-right">
