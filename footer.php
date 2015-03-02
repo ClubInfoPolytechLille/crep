463a19bdc999a002d272ea3ee6d004a05467e479
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-02 23:47:39 +0100

    Alt text footer

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
           <li>Sponsorisé par Polytech Lille</li>
-          <li><a href="http://www.polytech-lille.fr/ecole-d-ingenieurs.html"><img src="img/polytechlille.png" /></a></li>
+          <li><a href="http://www.polytech-lille.fr/ecole-d-ingenieurs.html"><img src="img/polytechlille.png" alt="Logo de Poltytech Lille"/></a></li>
         </ul>
       </div>
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
           <li>Et par l'Université de Lille 1</li>
-          <li><a href="http://www.univ-lille1.fr/"><img src="img/lille1.png" /></a></li>
+          <li><a href="http://www.univ-lille1.fr/"><img src="img/lille1.png" alt="Logo de l'Université de Lille 1 - Sciences et Technologies"/></a></li>
         </ul>
       </div>  
     </div>
