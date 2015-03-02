b8d34eaabf63f24d0e9d5f8bc942b67bb775ebdd
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-02 16:13:29 +0100

    Moved map initialization call

--- js/crep.js
+++ js/crep.js
@@ -4,7 +4,9 @@ function loadDoc(ev) {
         $.get(location + '?c', function (data) {
             mainContainer = $("#mainContainer")
             mainContainer.html(data)
+            // POST
             dynamiseLinks(mainContainer)
+            pageSpecific(location)
         })
         return false
     }
@@ -14,6 +16,30 @@ function dynamiseLinks(el) {
     $("a", el).click(loadDoc)
 }
 
+function pageSpecific(location) {
+    if (location.indexOf('contact') >= 0) {
+        initializeMap()
+    }
+}
+
 $(document).ready(function () {
     dynamiseLinks(document)
+    pageSpecific(window.location.href)
 })
+
+function initializeMap() {
+    var mapCanvas = document.getElementById('map-canvas');
+    mapCanvas.innerHTML = ''
+    var polytechPos = new google.maps.LatLng(50.6074998, 3.1373338);
+    var mapOptions = {
+        center: polytechPos,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    var marker = new google.maps.Marker({
+        position: polytechPos,
+        map: map,
+        title: "Polytech Lille"
+    });
+}
--- pages/contact.php
+++ pages/contact.php
@@ -7,24 +7,3 @@
 <div id="map-canvas">
     <img src="img/polyMap.png"/>
 </div>
-<script>
-	function initialize()
-	{
-		var mapCanvas = document.getElementById('map-canvas');
-        mapCanvas.innerHTML = ''
-        var polytechPos = new google.maps.LatLng(50.6074998, 3.1373338);
-		var mapOptions =
-		{
-			center: polytechPos,
-			zoom: 16,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-        var marker = new google.maps.Marker({
-            position: polytechPos,
-            map: map,
-            title:"Polytech Lille"
-        });
-	}
-    initialize()
-</script>
