3a30fef5d4d115ddedf62d3ee25d330ac09e8557
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-03 08:50:36 +0100

    Better remplacement for map with a link

--- pages/contact.php
+++ pages/contact.php
@@ -5,5 +5,5 @@
 &nbsp;
 &nbsp;
 <div id="map-canvas">
-    <img src="img/polyMap.png"/>
+    <a href="https://www.google.fr/maps/place/Polytech+Lille/@50.607584,3.136977,15z/data=!4m2!3m1!1s0x0:0xcc566fb8dc443187"><img src="img/polyMap.png"/></a>
 </div>
