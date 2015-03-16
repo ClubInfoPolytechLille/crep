7edae8f114de0c97db5d62351b3adbb231e1acfa
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-16 18:23:33 +0100

    Better 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,5 @@
-<h2>Page non-trouvée !</h2>
+<div class="container">
+    <h2>Page non-trouvée !</h2>
+    <img alt="WALL-E tout cassé" src="img/404.png" style="max-height: 400px; max-width: 90%"/>
+    <p>Désolé !</p>
+</div>
