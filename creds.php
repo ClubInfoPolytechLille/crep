4e953d060283e3f796fa6405ad5d3d3f1da5c2a2
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-16 17:36:04 +0100

    At this point it won't make much of a difference

--- creds.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("__MYSQL_USERNAME__", "crep");
-define("__MYSQL_PASSWORD__", "lescrepcestcool");
-define("__MYSQL_HOSTNAME__", "cambraisis.escaut.net");
-define("__MYSQL_DATABASE__", "crep");
-mb_internal_encoding('UTF-8');
-mb_http_output('UTF-8');
-mb_http_input('UTF-8');
-?>
