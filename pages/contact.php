dd215af74ffcbfa995a3375acc67398d6b5b378e
henyxia <henyxia@live.fr>
2015-03-17 10:23:56 +0100

    Somes fixed New contact page

--- pages/contact.php
+++ pages/contact.php
@@ -1,7 +1,9 @@
-<div class="jumbotron">
-  <h1>Contact</h1>
-  <p>Pour toute information, vous pouvez contacter Mme Pichonnat</p>
+<div class="jumbotron" style="min-height: 250px;">
+	<img src="img/contact.jpg" align="left" style="margin-right: 20px;">
+	<h1>Contact</h1>
+	<p>Pour toute information, vous pouvez contacter Mme Pichonat</p>
 </div>
+
 <div id="map-canvas">
     <a href="https://www.google.fr/maps/place/Polytech+Lille/@50.607584,3.136977,15z/data=!4m2!3m1!1s0x0:0xcc566fb8dc443187"><img src="img/polyMap.png"/></a>
 </div>
