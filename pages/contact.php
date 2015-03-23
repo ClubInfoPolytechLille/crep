377d5daa469710557c565d419f040d2380ba0289
henyxia <henyxia@live.fr>
2015-03-23 10:07:37 +0100

    Less information

--- pages/contact.php
+++ pages/contact.php
@@ -2,10 +2,13 @@
 	<img src="img/contact.jpg" align="left" style="margin-right: 20px;">
 	<h1>Contact</h1>
 	<p>Pour toute information, vous pouvez contacter Mme Pichonat</p>
+<?php
+/*
 	<p><span class="glyphicon glyphicon-envelope"></span> emmanuelle.pichonat@polytech-lille.fr</p>
 	<p><span class="glyphicon glyphicon-map-marker"></span> Polytech Lille</p>
 	<p>2, Bd Langevin</p>
 	<p>59655 Villeneuve d'Ascq Cedex</p>
+*/?>
 	<p><span class="glyphicon glyphicon-phone-alt"></span> 03 28 76 74 47</p>
 </div>
 
