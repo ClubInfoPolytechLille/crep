722c6952dd797dd5c230acc58c0f3cb026d731b1
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-23 17:15:05 +0100

    schools: Nicer exception handling

--- pages/schools.php
+++ pages/schools.php
@@ -1,7 +1,25 @@
-<?php
-require_once("creds.php");
-?>
 <h2>Liste écoles participantes</h2>
+<?php
+require_once ("creds.php");
+
+try {
+    
+    $link = @mysql_connect(__MYSQL_HOSTNAME__, __MYSQL_USERNAME__, __MYSQL_PASSWORD__);
+
+    if (!$link) {
+    	throw new Exception('Impossible de se connecter : ' . mysql_error());
+    }
+    
+    if (!mysql_select_db('crep', $link)) {
+        throw new Exception('Selection de la base de donnees impossible');
+    }
+    mysql_query("SET NAMES 'utf8'");
+    $requete = "SELECT DISTINCT `nom`, `circonscription`, `adresse`, `enseignant`, `contact` FROM `school` ORDER BY nom ASC";
+
+    $resultat = mysql_query($requete);
+
+    mysql_close($link);
+?>
 
 <center>
 	<table class="table table-striped table-responsive table-bordered">
@@ -11,48 +29,54 @@ require_once("creds.php");
 					<th>Nom de l'école</th>
 					<th>Circonscription</th>
 					<th>Contact</th>
-<!-- 				<th>Enseignant</th>
-					<th>Contact</th> -->
+<?php /*
+					<th>Enseignant</th>
+					<th>Contact</th>
+*/ ?>
 				</tr>
 			</thead>
 		</div >
 		<tbody>
 <?php
-$link = mysql_connect(__MYSQL_HOSTNAME__, __MYSQL_USERNAME__, __MYSQL_PASSWORD__)
-	or die("Impossible de se connecter : " . mysql_error());
-
-if(!mysql_select_db('crep', $link))
-{
-	echo 'Selection de la base de donnees impossible';
-	exit;
-}
-mysql_query("SET NAMES 'utf8'");
-$requete = "SELECT DISTINCT `nom`, `circonscription`, `adresse`, `enseignant`, `contact` FROM `school` ORDER BY nom ASC";
-$resultat = mysql_query($requete);
-
-while ($row = mysql_fetch_assoc($resultat))
-{
-	echo '<tr>';
-	echo '<td>';
-	echo '<p>'.$row['nom'].'</p>';
-	echo '</td>';
-	echo '<td>';
-	echo '<p>'.$row['circonscription'].'</p>';
-	echo '</td>';
-	echo '<td>';
-	echo '<p>'.$row['adresse'].'</p>';
-	echo '</td>';
-	// echo '<td>';
-	// echo '<p>'.$row['enseignant'].'</p>';
-	// echo '</td>';
-	// echo '<td>';
-	// echo '<p>'.$row['contact'].'</p>';
-	// echo '</td>';
-echo '</tr>';
-}
-
-mysql_close($link);
+    
+    while ($row = mysql_fetch_assoc($resultat)) {
+        echo '<tr>';
+        echo '<td>';
+        echo '<p>' . $row['nom'] . '</p>';
+        echo '</td>';
+        echo '<td>';
+        echo '<p>' . $row['circonscription'] . '</p>';
+        echo '</td>';
+        echo '<td>';
+        echo '<p>' . $row['adresse'] . '</p>';
+        echo '</td>';
+        
+        // echo '<td>';
+        // echo '<p>'.$row['enseignant'].'</p>';
+        // echo '</td>';
+        // echo '<td>';
+        // echo '<p>'.$row['contact'].'</p>';
+        // echo '</td>';
+        echo '</tr>';
+    }
 ?>
+
 		</tbody>
 	</table>
 </center>
+<?php
+}
+catch(Exception $e) {
+?>
+<div class="alert alert-danger" role="alert">
+Impossible d'afficher la liste des écoles participantes.<br/>
+Merci de rééssayer ultérieurement.
+</div>
+<!--
+<?php
+echo $e;
+?>
+-->
+<?php
+}
+?>
