f43f5448b41d02b2d55274c1a6b03acc065274e4
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-25 19:49:39 +0100

    Centered sponsors
    Not sure if it looks good

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -1,5 +1,19 @@
-<h2>Sponsors</h2>
-<h3>LEGO Education</h3>
-<img class="logo" alt="Logo LEGO Education" src="img/legoEducation.png" />
-<h3>Polytech Instrumentation</h3>
-<img class="logo" alt="Logo Polytech Instrumentation" src="img/polytechInstrumentation.png" />
+<div id="sponsors" class="container text-center">
+  <h2>Sponsors</h2>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="col-md-6">
+        <ul class="nav nav-pills nav-stacked">
+          <h3>LEGO Education</h3>
+          <li><img class="logo" alt="Logo LEGO Education" src="img/legoEducation.png" /></li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <ul class="nav nav-pills nav-stacked">
+          <h3>Polytech Instrumentation</h3>
+          <li><img class="logo" alt="Logo Polytech Instrumentation" src="img/polytechInstrumentation.png" /></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
