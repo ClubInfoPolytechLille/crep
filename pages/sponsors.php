372c17053444a4b4360c1764dab786457e270cb2
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-25 19:45:47 +0100

    Added sponsors to page

---
+++ pages/sponsors.php
@@ -0,0 +1,5 @@
+<h2>Sponsors</h2>
+<h3>LEGO Education</h3>
+<img class="logo" alt="Logo LEGO Education" src="img/legoEducation.png" />
+<h3>Polytech Instrumentation</h3>
+<img class="logo" alt="Logo Polytech Instrumentation" src="img/polytechInstrumentation.png" />
