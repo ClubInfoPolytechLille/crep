181b2b9189606763b981f6e50554c21a0e964142
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-30 19:18:38 +0200

    + 33 works better than 0033

--- pages/contact.php
+++ pages/contact.php
@@ -3,7 +3,7 @@
 	<h1>Contact</h1>
 	<p>Pour toute information, vous pouvez contacter Mme Pichonat</p>
 	<p><span class="glyphicon glyphicon-envelope"></span> <a href="mailto:emmanuelle.pichonat@polytech-lille.fr">emmanuelle.pichonat@polytech-lille.fr</a></p>
-	<p><span class="glyphicon glyphicon-phone-alt"></span> <a href="tel:0033328767447">03 28 76 74 47</a></p>
+	<p><span class="glyphicon glyphicon-phone-alt"></span> <a href="tel:+33328767447">03 28 76 74 47</a></p>
 	<p><span class="glyphicon glyphicon-map-marker"></span> Polytech Lille</p>
 	<p>2, Bd Langevin</p>
 	<p>59655 Villeneuve d'Ascq Cedex</p>
