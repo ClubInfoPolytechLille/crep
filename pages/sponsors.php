444b66fa06c309901d2695a74287e92ad291bfcd
Jean Wasilewski <henyxia@live.fr>
2015-03-31 08:56:17 +0200

    Added Oxybul

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -13,7 +13,15 @@
           <h3>Polytech Instrumentation</h3>
           <li><a href="http://www.polytech-instrumentation.fr/" target="_blank"><img class="logo" alt="Logo Polytech Instrumentation" src="img/polytechInstrumentation.png" /></a></li>
         </ul>
+	  </div>
+	  <div class="col-md-3">
       </div>
-    </div>
+	  <div class="col-md-6">
+        <h3>Oxybul</h3>
+        <a href="http://www.oxybul.com" target="_blank"><img class="logo" alt="Logo Oxybul" src="img/oxybul.png" /></a>
+      </div>
+	  <div class="col-md-3">
+      </div>
+	</div>
   </div>
 </div>
