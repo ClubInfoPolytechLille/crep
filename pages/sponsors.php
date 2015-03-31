1e9f405588a12cf8396d4e7a3ee5534f4a4bc4a3
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-03-31 20:50:13 +0200

    Semantics

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -4,13 +4,13 @@
     <div class="col-lg-12">
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
-          <h3>LEGO Education</h3>
+          <li><h3>LEGO Education</h3></li>
           <li><a href="http://education.lego.com/" target="_blank"><img class="logo" alt="Logo LEGO Education" src="img/legoEducation.png" /></a></li>
         </ul>
       </div>
       <div class="col-md-6">
         <ul class="nav nav-pills nav-stacked">
-          <h3>Polytech Instrumentation</h3>
+          <li><h3>Polytech Instrumentation</h3></li>
           <li><a href="http://www.polytech-instrumentation.fr/" target="_blank"><img class="logo" alt="Logo Polytech Instrumentation" src="img/polytechInstrumentation.png" /></a></li>
         </ul>
 	  </div>
@@ -18,7 +18,7 @@
       </div>
 	  <div class="col-md-6">
           <ul class="nav nav-pills nav-stacked">
-            <h3>Oxybul</h3>
+            <li><h3>Oxybul</h3></li>
             <li><a href="http://www.oxybul.com" target="_blank"><img class="logo" alt="Logo Oxybul" src="img/oxybul.png" /></a></li>
             </ul>
       </div>
