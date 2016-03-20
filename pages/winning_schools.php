f10066d7a5e5ab41b1e862c3460171899c65887d
Marianne <marinana@mac.com>
2016-03-20 23:55:02 +0100

    creation de la page des vainqueurs

---
+++ pages/winning_schools.php
@@ -0,0 +1,45 @@
+<div class="container-fluid">
+  <!-- Titre de la page -->
+  <div class="row"><div class="col-md-12"><h1>Vainqueurs des éditions précédentes</h1></div></div>
+
+  <br/><br/>
+  <!-- Les tableaux -->
+
+  <!-- Vainqueurs de 2015 -->
+  <div class="row"><div class="col-md-12">
+    <table class="table table-striped table-bordered table-responsive">
+      <tr>
+        <th colspan='2'>CREP du 9 avril 2015</th>
+      </tr>
+      <tr>
+        <td style="width:60%">École Samain</td>
+        <td style="width:40%">Lambersart</td>
+      </tr>
+      <tr>
+        <td>École Condorcet</td>
+        <td>Willems</td>
+      </tr>
+    </table>
+  </div></div>
+
+  <br/>
+  
+  <!-- Vainqueurs de 2014 -->
+  <div class="row"><div class="col-md-12">
+    <table class="table table-striped table-bordered table-responsive">
+      <tr>
+        <th colspan='2'>CREP du 15 mai 2014</th>
+      </tr>
+      <col style="width:80%" />
+      <tr>
+        <td style="width:60%">École Jules Ferry</td>
+        <td style="width:40%">Wambrechies</td>
+      </tr>
+      <tr>
+        <td>École Louise de Bettignies</td>
+        <td>Lambersart</td>
+      </tr>
+    </table>
+  </div></div>
+
+</div>
